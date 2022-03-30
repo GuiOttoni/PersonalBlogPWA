c4589d4b085e9bcd09a28b986bf771493d129a22
Guilherme Ottoni Escarelli De Alencar <guiottonie@gmail.com>
2022-03-29 22:17:17 -0300

    add blogPost + stringExtension

---
+++ Models/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using PersonalBlogPWA.Utils;
+
+namespace PersonalBlogPWA.Models
+{
+    public class BlogPost
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Link { get { return ShortDescription.UrlFriendly(50); } }
+
+        
+    }
+    
+
+}
